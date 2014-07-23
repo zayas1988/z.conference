0bdd05b4cec7f12f5b666acb7d1ded98123aed73
malfeev <malafeev@reg.ru>
2014-07-23 15:22:14 +0400

    fixes

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140722150016) do
+ActiveRecord::Schema.define(version: 20140723093954) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "calls", force: true do |t|
+    t.string   "raw"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "meets", force: true do |t|
     t.boolean  "ok"
