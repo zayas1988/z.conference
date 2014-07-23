b41e04175d8a0360f263e7d114e931921a2465e5
zayas <zayas1988@gmail.com>
2014-07-23 16:58:03 +0400

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,10 @@ class User < ActiveRecord::Base
   has_many :numbers
   accepts_nested_attributes_for :numbers
   def call(user)
-  	for user.numbers.each do |number|
-  		if number.active?
-  		  ff= File.new("/tmp/#{number.num}.call","w")
-  		  ff.puts("Channel: Local/89093625244@from-internal
+    for user.numbers.each do |number|
+      if number.active?
+        ff= File.new("/tmp/#{number.num}.call","w")
+        ff.puts("Channel: Local/#{number.num}@from-internal
 MaxRetries: 0
 RetryTime: 600
 WaitTime: 30
@@ -13,12 +13,9 @@ Context: from-internal
 Extension: 1292
 Priority: 1
 ")
-          ff.close
-          result =  %x(sudo -u asterisk cp /tmp/#{number.num}.call /var/spool/asterisk/outgoing/)
+        ff.close
+        result =  %x(sudo -u asterisk cp /tmp/#{number.num}.call /var/spool/asterisk/outgoing/)
       end
-  	end
+    end
   end
-
-
-
 end
