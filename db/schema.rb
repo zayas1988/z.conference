a9b6803359da2164bd0f51d3e6a5daa8008a2d6e
Malafeev Anton <malafeev@reg.ru>
2015-02-10 11:15:11 +0400

    add accounts controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140723093954) do
+ActiveRecord::Schema.define(version: 20150209131733) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "accounts", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "accounts_conferences", force: true do |t|
+    t.integer "account_id"
+    t.integer "conference_id"
+  end
+
   create_table "calls", force: true do |t|
     t.string   "raw"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "conferences", force: true do |t|
+    t.string   "num"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
